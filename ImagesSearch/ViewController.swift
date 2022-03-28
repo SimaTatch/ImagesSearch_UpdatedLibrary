c8052c920fcff2c4d9689a6870db18b7a8f8dbd9
Sima <sima_tatchenkova@mail.ru>
2022-03-28 15:13:56 +0400

    Initial Commit

---
+++ ImagesSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagesSearch
+//
+//  Created by Серафима  Татченкова  on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
