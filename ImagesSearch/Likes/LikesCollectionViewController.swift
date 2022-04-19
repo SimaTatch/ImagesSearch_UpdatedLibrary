3397c7bc73bb0ea4780bbfa1225c9940678e14bb
Sima <sima_tatchenkova@mail.ru>
2022-04-19 21:05:05 +0400

    TOCropViewController is added

--- ImagesSearch/Likes/LikesCollectionViewController.swift
+++ ImagesSearch/Likes/LikesCollectionViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import CropViewController
 
-class LikesCollectionViewController: UICollectionViewController {
+class LikesCollectionViewController: UICollectionViewController, CropViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    //MARK: - Private objects setup
     var photos = [UnsplashPhoto]()
+    
+    private let myImageView = UIImageView()
+    
+    private var myImage: UIImage?
+    private var croppingStyle = CropViewCroppingStyle.default
+    
+    private var croppedRect = CGRect.zero
+    private var croppedAngle = 0
     
     private var selectedImages = [UIImage]()
     
@@ -21,6 +31,10 @@ class LikesCollectionViewController: UICollectionViewController {
         return UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteLikesPhotos))
     }()
     
+    private lazy var editBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editLikesPhotos))
+    }()
+    
     private let enterSearchTermLabel: UILabel = {
         let label = UILabel()
         label.text = "You haven't add a photos yet"
@@ -30,6 +44,7 @@ class LikesCollectionViewController: UICollectionViewController {
         return label
     }()
 
+    //MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,17 +53,72 @@ class LikesCollectionViewController: UICollectionViewController {
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         collectionView.allowsMultipleSelection = true
         
+        
+        myImageView.isUserInteractionEnabled = true
+        myImageView.contentMode = .scaleAspectFit
+        if #available(iOS 11.0, *) {
+            myImageView.accessibilityIgnoresInvertColors = true
+        }
+
+        view.addSubview(myImageView)
+        
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.minimumInteritemSpacing = 1
         layout.minimumLineSpacing = 1
         
         setupEnterLabel()
         setupNavigationBar()
-       
+        
     }
     
+    //MARK: - viewDidLayoutSubviews()
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layoutImageView()
+    }
+    
+    
+    //MARK: - cropVC func
+    public func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        self.croppedRect = cropRect
+        self.croppedAngle = angle
+        updateImageViewWithImage(image, fromCropViewController: cropViewController)
+    }
+    
+    public func cropViewController(_ cropViewController: CropViewController, didCropToCircularImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        self.croppedRect = cropRect
+        self.croppedAngle = angle
+        updateImageViewWithImage(image, fromCropViewController: cropViewController)
+    }
+    
+    public func updateImageViewWithImage(_ image: UIImage, fromCropViewController cropViewController: CropViewController) {
+        myImageView.image = image
+        layoutImageView()
+        self.refresh()
+
+        
+        if cropViewController.croppingStyle != .circular {
+            myImageView.isHidden = true
+            
+            self.collectionView.reloadData()
+            
+            cropViewController.dismissAnimatedFrom(self, withCroppedImage: image,
+                                                   toView: myImageView,
+                                                   toFrame: CGRect.zero,
+                                                   setup: { self.layoutImageView() },
+                                                   completion: {
+                                                    self.myImageView.isHidden = false })
+        }
+        else {
+            self.myImageView.isHidden = false
+            cropViewController.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Supporting func
     private func undateNavButtonsState() {
         trashBarButtonItem.isEnabled = numberOfSelectedPhotos > 0
+        editBarButtonItem.isEnabled = numberOfSelectedPhotos == 1
     }
     
     func refresh() {
@@ -58,7 +128,6 @@ class LikesCollectionViewController: UICollectionViewController {
     }
     
     // MARK: - NavigationItems action
-
     @objc private func deleteLikesPhotos() {
         let selectedPhotos = collectionView.indexPathsForSelectedItems?.reduce([], { (photosss, indexPath) -> [UnsplashPhoto] in
             var mutablePhotos = photosss
@@ -76,7 +145,6 @@ class LikesCollectionViewController: UICollectionViewController {
                     photo.urls == selected.urls
                 }) ?? false
             }
-            
             self.collectionView.reloadData()
             self.refresh()
         }
@@ -87,8 +155,65 @@ class LikesCollectionViewController: UICollectionViewController {
         present(alertController, animated: true)
     }
     
-    // MARK: - Setup UI Elements
+    @objc func editLikesPhotos() {
+        cropCustomImage(image: selectedImages.first!)
+     }
     
+    //MARK: - imagePickerController func
+    func cropCustomImage(image: UIImage) {
+        let imagePicker = UIImagePickerController()
+        let cropController = CropViewController(croppingStyle: croppingStyle, image: image)
+        cropController.delegate = self
+        self.myImage = image
+        
+        //If profile picture, push onto the same navigation stack
+        if croppingStyle == .circular {
+            if imagePicker.sourceType == .camera {
+                imagePicker.dismiss(animated: true, completion: {
+                    self.present(cropController, animated: true, completion: nil)
+                })
+            } else {
+                imagePicker.pushViewController(cropController, animated: true)
+            }
+        }
+        else { //otherwise dismiss, and then present from the main controller
+            imagePicker.dismiss(animated: true, completion: {
+                self.present(cropController, animated: true, completion: nil)
+//                self.navigationController!.pushViewController(cropController, animated: true)
+            })
+        }
+    }
+
+    
+    //MARK: - layoutImageView()
+    public func layoutImageView() {
+        guard myImageView.image != nil else { return }
+
+        let padding: CGFloat = 20.0
+
+        var viewFrame = self.view.bounds
+        viewFrame.size.width -= (padding * 2.0)
+        viewFrame.size.height -= ((padding * 2.0))
+
+        var imageFrame = CGRect.zero
+        imageFrame.size = myImageView.image!.size;
+
+        if myImageView.image!.size.width > viewFrame.size.width || myImageView.image!.size.height > viewFrame.size.height {
+            let scale = min(viewFrame.size.width / imageFrame.size.width, viewFrame.size.height / imageFrame.size.height)
+            imageFrame.size.width *= scale
+            imageFrame.size.height *= scale
+            imageFrame.origin.x = (self.view.bounds.size.width - imageFrame.size.width) * 0.5
+            imageFrame.origin.y = (self.view.bounds.size.height - imageFrame.size.height) * 0.5
+            myImageView.frame = imageFrame
+        }
+        else {
+            self.myImageView.frame = imageFrame;
+            self.myImageView.center = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY)
+        }
+    }
+    
+    
+    // MARK: - Setup UI Elements
     private func setupEnterLabel() {
         collectionView.addSubview(enterSearchTermLabel)
         enterSearchTermLabel.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor).isActive = true
@@ -98,12 +223,12 @@ class LikesCollectionViewController: UICollectionViewController {
     private func setupNavigationBar() {
         let titleLabel = UILabel(text: "FAVOURITES", font: .systemFont(ofSize: 15, weight: .medium), textColor: #colorLiteral(red: 0.5, green: 0.5, blue: 0.5, alpha: 1))
         navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
-        navigationItem.rightBarButtonItem = trashBarButtonItem
+        navigationItem.rightBarButtonItems = [trashBarButtonItem, editBarButtonItem]
         trashBarButtonItem.isEnabled = false
+        editBarButtonItem.isEnabled = false
     }
     
     // MARK: - UICollectionViewDataSource
-    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         enterSearchTermLabel.isHidden = photos.count != 0
         return photos.count
@@ -122,8 +247,9 @@ class LikesCollectionViewController: UICollectionViewController {
         guard let image = cell.myImageView.image else { return }
         selectedImages.append(image)
         
+
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         undateNavButtonsState()
         let cell = collectionView.cellForItem(at: indexPath) as! LikesCollectionViewCell
@@ -135,7 +261,6 @@ class LikesCollectionViewController: UICollectionViewController {
 }
 
     // MARK: - UICollectionViewDelegateFlowLayout
-
 extension LikesCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
